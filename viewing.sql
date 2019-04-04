924e79fe0aec41687dc776ff8cd1d6b6097f6fc5
satanza28 <48791800+satanza28@users.noreply.github.com>
2019-04-04 17:33:37 +0700

    Add files via upload

---
+++ viewing.sql
@@ -0,0 +1,7 @@
+
+create table viewing(id int primary key auto_increment,
+     client_no varchar(4) not null,
+     property_no varchar(4) not null,
+     view_date date not null,
+     comment text
+     );
